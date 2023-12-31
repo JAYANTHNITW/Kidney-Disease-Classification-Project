dcdeca014c1ee0893de6377dfb8a258195fb30bc
Kothapalli Jayanth <121042707+JAYANTHNITW@users.noreply.github.com>
2023-12-31 11:38:09 +0530

    model training added

---
+++ src/CNNclassifier/components/prepare_base_model.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+from tensorflow import keras
+# from keras.applications.resnet50 import ResNet50
+from src.CNNclassifier.entity.config_entity import PrepareBaseModelConfig
+
+
+class PrepareBaseModel:
+    def __init__(self,config:PrepareBaseModelConfig):
+        self.config = config
+
+    def get_base_model(self):
+        self.model =  keras.applications.vgg16.VGG16(
+            input_shape = self.config.params_image_size,
+            weights= self.config.params_weights,
+            include_top = self.config.params_include_top
+        )
+        self.save_model(path=self.config.base_model_path,model= self.model)
+
+    @staticmethod
+    def _prepare_full_model(model,classes,freeze_all,freeze_till,Learning_rate):
+        if freeze_all:
+            for layer in model.layers:
+                model.trainable = False
+        elif (freeze_till is not None) and (freeze_till>0):
+            for layer in model.layers[:freeze_till]:
+                model.trainable = False
+        flatten_in = keras.layers.Flatten()(model.output)
+        prediction = keras.layers.Dense(units=classes,activation="softmax")(flatten_in)
+
+        full_model = keras.models.Model(inputs=model.input,
+                                        outputs=prediction)
+        
+        full_model.compile(
+            optimizer = keras.optimizers.Adam(learning_rate=Learning_rate),
+            loss = keras.losses.CategoricalCrossentropy(),
+            metrics = ["accuracy"]
+            )
+        
+        full_model.summary()
+        return full_model
+
+    def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model = self.model,
+            classes = self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            Learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path,model=self.full_model)
+
+
+    @staticmethod
+    def save_model(path:Path,model: tf.keras.Model):
+        model.save(path)
+
+

787c4c1975a432068e5cda9866cc42cc55f60746
Kothapalli Jayanth <121042707+JAYANTHNITW@users.noreply.github.com>
2023-12-31 11:50:36 +0530

    model training done

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 from src.CNNclassifier import logger
+from src.CNNclassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.CNNclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.CNNclassifier.pipeline.stage_3_model_training import ModelTrainingPipeline
 
-logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_training = ModelTrainingPipeline()
+   model_training.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e

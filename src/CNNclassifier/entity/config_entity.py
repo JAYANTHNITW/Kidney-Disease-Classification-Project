a19ded5cf6b7cffa123ce824839103bb2f8c4d9a
Kothapalli Jayanth <121042707+JAYANTHNITW@users.noreply.github.com>
2023-12-31 11:42:27 +0530

    updataed_training

---
+++ src/CNNclassifier/entity/config_entity.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+#dataclass: we used this so that can access the class from other files as well.
+from pathlib import Path
+#entity: it is the return type of a classes
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+ 
+ 
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_is_augmentation: bool
+    params_image_size: list

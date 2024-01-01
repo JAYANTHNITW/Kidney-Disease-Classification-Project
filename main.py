b6c7202b3020bcb954cbbfc1591efe92c30cbdd2
Kothapalli Jayanth <121042707+JAYANTHNITW@users.noreply.github.com>
2024-01-01 14:21:15 +0530

    cicd pipeline added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from src.CNNclassifier import logger
-from src.CNNclassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
-from src.CNNclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from src.CNNclassifier.pipeline.stage_3_model_training import ModelTrainingPipeline
+from CNNclassifier import logger
+from CNNclassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNNclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNNclassifier.pipeline.stage_3_model_training import ModelTrainingPipeline
+from CNNclassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -34,5 +35,18 @@ try:
    model_training.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        evaluate = EvaluationPipeline()
+        evaluate.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+    except Exception as e:
         logger.exception(e)
         raise e
